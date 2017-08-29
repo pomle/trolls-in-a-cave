19300d435f2cc6bf0baced652cc2f34342711561
Pontus Persson <pontus.alexander@gmail.com>
2017-08-29 12:16:45 -0700

    Make an intro.

--- src/game/index.js
+++ src/game/index.js
@@ -1,5 +1,22 @@
 import { Game } from '@snakesilk/engine';
+import { XMLLoader, Parser } from '@snakesilk/xml-loader';
 
 export function createGame() {
-    return new Game();
+    function loadScene(url) {
+        return loader.asyncLoadXML(url)
+        .then(doc => doc.children[0])
+        .then(node => sceneParser.getScene(node))
+        .then(context => {
+            console.log('Scene', context);
+            trolls.setScene(context.scene);
+        });
+    }
+
+    const trolls = new Game();
+    const loader = new XMLLoader(trolls);
+    const sceneParser = new Parser.SceneParser(loader);
+
+    loadScene('/resources/intro.xml');
+
+    return trolls;
 }
